1911c645775b0dc07ecf04070872f3b3b713f697
Anonna <136353692+0xanonna111@users.noreply.github.com>
2026-01-22 21:30:36 +0600

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,19 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: process.env.RPC_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+    // Localhost for testing
+    hardhat: {
+      chainId: 1337
+    }
+  },
+  paths: {
+    artifacts: "./artifacts"
+  }
+};
